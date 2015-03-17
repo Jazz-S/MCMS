c33e97db457422ef2370787f7f3f743512d2697b
cinnabarq <cinnabarq@gmail.com>
2015-03-17 16:44:00 -0700

    second update: finished step 3

--- MCMS/CreatureListViewController.m
+++ MCMS/CreatureListViewController.m
@@ -9,6 +9,8 @@
 #import "CreatureListViewController.h"
 #import "MagicalCreature.h"
 @interface CreatureListViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UITextField *creatureTextField;
 
 @end
 
@@ -24,16 +26,29 @@
 
     self.creatures =[NSMutableArray arrayWithObjects:creature1,creature2, creature3, nil];
 }
+
+
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID"];
     MagicalCreature *creatureRow =[self.creatures objectAtIndex:indexPath.row];
-    cell.textLabel.text = creatureRow.name;
+    cell.textLabel.text = creatureRow.fullName;
     return cell;
 }
+
+
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return self.creatures.count;
+}
+
+- (IBAction)addCreature:(UIButton *)sender {
+    NSString *creatureName = self.creatureTextField.text;
+    MagicalCreature *newCreature = [[MagicalCreature alloc]initWithName:creatureName];
+    [self.creatures addObject:newCreature ];
+    [self.tableView reloadData];
+    [self.creatureTextField resignFirstResponder];
+    self.creatureTextField.text= @"";
 }
 
 @end
